e601ca1893ea3a1ba13136b853cdd0106f74b860
Xeth Hung <xcwhung@ogcio.gov.hk>
2020-09-22 15:52:42 +0800

    update name

--- image_build.sh
+++ image_build.sh
@@ -1,3 +1,3 @@
 wget https://download.java.net/openjdk/jdk11/ri/openjdk-11+28_linux-x64_bin.tar.gz
 tar xvf openjdk-11+28_linux-x64_bin.tar.gz
-docker build  --build-arg JAVA_VERSION=jdk-11 --tag hi .
+docker build  --build-arg JAVA_VERSION=jdk-11 --tag jdk11-runner .
